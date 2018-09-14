a4150607f0a2970c22843c1170866235a90fc104
AnneDeGraaf <25728122+AnneDeGraaf@users.noreply.github.com>
2018-09-14 10:26:34 -0700

    Update example_deflection_locomotive.py
    added pickle

--- example_deflection_locomotive.py
+++ example_deflection_locomotive.py
@@ -2,27 +2,28 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
+import pickle
 
 
-## LOCOMOTIVE RUNNING ACROSS RAILWAY BRIDGE
+## PASSENGER TRAIN RUNNING ACROSS RAILWAY BRIDGE
 
 
 # input vehicle
-mass = 			# kg
-velocity = 		# m/s
-spring =  		# N/m
-damp =  		# N/m/s
-wbase =  		# m
-inertia = 		# kg.m^2
+mass = 28950.0		# kg
+velocity = 55.6 	# m/s
+spring = 8.8e5 		# N/m
+damp = 6.0e4 		# N.s/m
+wbase = 12.6		# m
+inertia = 60312.5	# kg.m^2
 
 # input beam
-span =  		# m
-area =  		# m^2
-EI =  			# N.m^2
-density =  		# kg/m^3
+span = 105.0		# m
+area = 26.15 		# m^2
+EI = 6.28e12		# N.m^2
+density = 2400.0	# kg/m^3
 excitationModes = 5
 
-g = 9.81 	# gravitational constant
+g = 9.81 		# gravitational constant
 
 massOffset = 0.5*mass*g/spring + 0.02 	# m
 
@@ -30,28 +31,29 @@ trajectory = span + 2*wbase 		# includes beam and land abutment on both ends
 
 # time steps
 lowestFreq = np.pi**2 / (span**2) * np.sqrt( EI/(density*area) )
+print(lowestFreq)
 dt = 0.01/lowestFreq
-time = np.arange(0, (span+wbase)/velocity+dt, dt)
-t1 = int((wbase/velocity)/dt)	# Q1 enters the beam (as time index)
-t2 = int((span/velocity)/dt)	# Q2 leaves the beam (as time index)
+time = np.arange(0, (span+wbase+300.0)/velocity+dt, dt)
+t1 = int((wbase/velocity)/dt)		# Q1 enters the beam (as time index)
+t2 = int((span/velocity)/dt)		# Q2 leaves the beam (as time index)
 
 # x steps
 dx = 0.1
 x = np.arange(0, trajectory+dx, dx)	
 x1 = int(wbase/dx)			# beginning of beam (as x index)
-x2 = int((wbase+span)/dx)	# end of beam (as x index)
+x2 = int((wbase+span)/dx)		# end of beam (as x index)
 
 # initializing empty arrays for the numerical calculation
-w = np.zeros((len(time), len(x)))		# w(t,x)
+w = np.zeros((len(time), len(x)))	# w(t,x)
 wDiff = np.zeros((len(time), len(x)))	# w'(t,x)			
 
 u = np.zeros(len(time))			# u(t)
 u[0] = 0.5*mass*g/spring 		# initial condition for u(0)
-uDiff = np.zeros(len(time)) 	# u'(t)
+uDiff = np.zeros(len(time)) 		# u'(t)
 
 theta = np.zeros(len(time))		# theta(t)
-theta[0] = 0  					# initial condition for theta(0)
-thDiff = np.zeros(len(time))	# theta'(t)
+theta[0] = 0  				# initial condition for theta(0)
+thDiff = np.zeros(len(time))		# theta'(t)
 
 Q1 = np.zeros(len(time))
 Q2 = np.zeros(len(time))
@@ -71,7 +73,7 @@ for t in np.arange(1, len(time)):
 
 	# determine x location (step) of forces at time t
 	xQ1[t] = min( x2, int(round((velocity*time[t])/dx)) )			# x step location of force 1
-	xQ2[t] = min( len(x)-1, int(round((velocity*time[t])/dx))+x1 )	# x step location of force 2
+	xQ2[t] = min( len(x)-1, int(round((velocity*time[t])/dx))+x1 )		# x step location of force 2
 
 
 	# determine intergrals for u(t) and theta(t) (trapezoidal method)
@@ -79,7 +81,7 @@ for t in np.arange(1, len(time)):
 	int_Q2 = np.trapz( (time[t]-time[0:t+1])*Q2[0:t+1], time[0:t+1] )
 
 	# determine vehicle displacement u(t) and pitch theta(t) and their derivatives (backward Euler method)
-	u[t] = mass*g/spring + 0.5*g*time[t]**2 - 1/mass*(int_Q1 + int_Q2)	
+	u[t] = 0.5*mass*g/spring + 0.5*g*time[t]**2 - 1/mass*(int_Q1 + int_Q2)	
 	uDiff[t] = (u[t]-u[t-1])/dt
 
 	theta[t] = 0.5*wbase/inertia*(int_Q2 - int_Q1)
@@ -122,28 +124,37 @@ for t in np.arange(1, len(time)):
 		Q2[t+1] = spring*( u[t] - 0.5*wbase*theta[t] - w[t, xQ2[t]] ) + damp*( uDiff[t] - 0.5*wbase*thDiff[t] - wDiff[t, xQ2[t]] )
 
 
+# storing the calculated data:
+with open('example_train.pkl', 'w') as f:  # Python 3: open(..., 'wb')
+    pickle.dump([w, u, theta, Q1, Q2], f)
+
+
 
 # ANIMATION:
 
 # initializing beam
-fig, ax = plt.subplots()
-ax.set_ylim([0.05, -0.1])
-beam, = ax.plot(x, w[0], color='k')
+fig, ax = plt.subplots(1,1)
+ax.set_ylim([30.0, -30.])
+ax.set_yticks(np.arange(30.0, -30.0, -10.0))
+ax.set_xlim([wbase, wbase+span])
+ax.set_xticks(np.arange(wbase, wbase+span+10, 10.0))
+
+beam, = ax.plot(x, w[0]*1000.0, color='dodgerblue', label='railway bridge')
 
 # initializing mass
-Q1Loc, = ax.plot(0.0, u[0]-massOffset, 'o', color='r')
-Q2Loc, = ax.plot(wbase, u[0]-massOffset, 'o', color='g')
+Q1Loc, = ax.plot(0.0, 0.02, 'o', color='slategrey', label='location Q1')
+Q2Loc, = ax.plot(wbase, 0.02, 'o', color='darkslategrey', label='location Q2')
 
 
 # Generate frames for animation
 def animationFrames(i):
-	beam.set_ydata( w[i] )
+	beam.set_ydata( w[i]*1000.0 )
 
 	Q1Loc.set_xdata(velocity*time[i])
-	Q1Loc.set_ydata(u[i]+0.5*wbase*theta[i]-massOffset)
+	# Q1Loc.set_ydata(u[i]*50.0-massOffset*50.0)
 
 	Q2Loc.set_xdata(velocity*time[i]+wbase)
-	Q2Loc.set_ydata(u[i]-0.5*wbase*theta[i]-massOffset)
+	# Q2Loc.set_ydata(u[i]*50.0-massOffset*50.0)
 
 	return(beam, Q1Loc, Q2Loc)
 
@@ -151,5 +162,3 @@ def animationFrames(i):
 # show animation
 ani = animation.FuncAnimation(fig, animationFrames, np.arange(0, len(w)), interval=1)
 plt.show()
-	
-
